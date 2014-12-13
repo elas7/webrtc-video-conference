3ca42ad715b2749aea6a534845829c2502351400
Seba <sebaest77@gmail.com>
2014-12-13 16:52:55 -0200

    make express and socket.io work together

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var static = require('node-static');
-var http = require('http');
-var file = new(static.Server)();
-var app = http.createServer(function (req, res) {
-    file.serve(req, res);
-}).listen(2013);
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.use('/static', express.static(path.join(__dirname, 'public/static')));
+
+app.get('/', function (req, res) {
+    res.sendfile((path.join(__dirname, 'public', 'index.html')));
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
