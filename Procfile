c0aa48793d77a47fe2e9e1b47ad532261f8a832f
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2014-12-23 19:17:08 -0200

    Fix Procfile and application entry point

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: node server.js
-
+web: node index.js
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var server = require('./server');
-var io = require('./signaling');
-var port = process.env.PORT || 8080;
+(function () {
+    'use strict';
 
-server.listen(port, function () {
-    console.log('Server listening at port %d', port);
-});
+    var server = require('./server'),
+        io = require('./signaling'),
+        port = process.env.PORT || 8080;
+
+    server.listen(port, function () {
+      console.log('Server listening at port %d', port);
+    });
+
+}).call(this);
